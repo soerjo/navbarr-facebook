42a09ee627e0457b24cb0dc1a9ae66d480605a4c
soerjo <ryohastomo@gmail.com>
2021-07-20 00:51:13 +0700

    push project facebook navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { FaBell } from "react-icons/fa";
+import { AiFillMessage } from "react-icons/ai";
+import { FaCaretDown } from "react-icons/fa";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navabar>
+      <NavItem icon={<FaPlus />} />
+      <NavItem icon={<FaBell />} />
+      <NavItem icon={<AiFillMessage />} />
+      <NavItem icon={<FaCaretDown />}>
+        <DropDownMenu />
+      </NavItem>
+    </Navabar>
+  );
+}
+
+function Navabar(props) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
+  );
+}
+
+function NavItem(props) {
+  const [open, setopen] = useState(false);
+
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button" onClick={() => setopen(!open)}>
+        {props.icon}
+      </a>
+      {open && props.children}
+    </li>
+  );
+}
+
+function DropDownMenu() {
+  function DropDownItem(props) {
+    return (
+      <a href="#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
+
+  return (
+    <div className="dropdown">
+      <DropDownItem> My Profile</DropDownItem>
+      <DropDownItem leftIcon="<"></DropDownItem>
     </div>
   );
 }
---
+++ src/assets/icons/icons.js
@@ -0,0 +1,13 @@
+import { FaBitcoin } from "react-icons/fa";
+import { FaAtlassian } from "react-icons/fa";
+import { FaDigitalOcean } from "react-icons/fa";
+
+export function IconFaBitcoin() {
+  return <FaBitcoin />;
+}
+export function IconFaAtlassian() {
+  return <FaAtlassian />;
+}
+export function IconFaDigitalOcean() {
+  return <FaDigitalOcean />;
+}
